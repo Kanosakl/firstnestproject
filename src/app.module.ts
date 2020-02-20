371c83b63929cc0c9380f0439df88605113e778f
Davion Teh <6722889+davionboy@users.noreply.github.com>
2020-02-20 16:31:55 +0800

    Added mongoose module

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,12 +3,34 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { OrdersModule } from './orders/orders.module';
-import { EasyconfigModule } from "nestjs-easyconfig";
+import { EasyconfigModule, EasyconfigService } from "nestjs-easyconfig";
 import { OrdersController } from './orders/orders.controller';
 import { OrdersService } from './orders/orders.service';
+import { MongooseModule } from "@nestjs/mongoose";
+
+const envConfigOption = { 
+  path: './config/.env', 
+  safe: true, 
+  sampleFilePath: './.env.sample' 
+};
+
+const easyConfigService = new EasyconfigService(envConfigOption)
 @Module({
-  imports: [OrdersModule, EasyconfigModule.register({ path: './config/.env', safe: true, sampleFilePath: './.env.sample' })],
+  imports: [
+    OrdersModule,
+    EasyconfigModule.register(envConfigOption),
+    MongooseModule.forRootAsync({      
+      useFactory: async () => ({
+        uri: easyConfigService.get('MONGO_URI'),
+        useNewUrlParser: true,
+        useUnifiedTopology:true,
+      }),
+    }),
+  ],
+  
   controllers: [AppController, OrdersController],
   providers: [AppService, OrdersService],
 })
-export class AppModule { }
+export class AppModule { 
+  constructor(private config: EasyconfigService) { }
+}
