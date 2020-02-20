1d7c4879bf5140fc5dd1f75c3353ab7c30c41fb9
Davion Teh <6722889+davionboy@users.noreply.github.com>
2020-02-20 15:07:43 +0800

    Added easyconfig module for env configuration

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,10 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-
+import { EasyconfigModule } from "nestjs-easyconfig";
 @Module({
-  imports: [],
+  imports: [EasyconfigModule.register({ path: './config/.env', safe: true, sampleFilePath: './.env.sample' })],
   controllers: [AppController],
   providers: [AppService],
 })
-export class AppModule {}
+export class AppModule { }
