bfe1e57b31a8fb8f7c5de4b8fd0027bc4e01ae71
Davion Teh <6722889+davionboy@users.noreply.github.com>
2020-02-20 15:43:51 +0800

    Added orders module (dummy stub for now)

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,14 @@
 import { Module } from '@nestjs/common';
+
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { OrdersModule } from './orders/orders.module';
 import { EasyconfigModule } from "nestjs-easyconfig";
+import { OrdersController } from './orders/orders.controller';
+import { OrdersService } from './orders/orders.service';
 @Module({
-  imports: [EasyconfigModule.register({ path: './config/.env', safe: true, sampleFilePath: './.env.sample' })],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [OrdersModule, EasyconfigModule.register({ path: './config/.env', safe: true, sampleFilePath: './.env.sample' })],
+  controllers: [AppController, OrdersController],
+  providers: [AppService, OrdersService],
 })
 export class AppModule { }
