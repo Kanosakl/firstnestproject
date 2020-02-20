9d06d9bcd745d6ccd8d2da00bede26f9a499e628
Davion Teh <6722889+davionboy@users.noreply.github.com>
2020-02-20 23:56:58 +0800

    Implemented fake transaction callback API

--- src/orders/orders.controller.ts
+++ src/orders/orders.controller.ts
@@ -1,4 +1,6 @@
 import { Controller, Get, Post, Body, UsePipes, ValidationPipe, HttpCode } from '@nestjs/common';
+import fetch from "node-fetch";
+
 import { OrdersService } from './orders.service';
 import { Order } from './order.interface';
 import { CreateOrderDto } from './create-order.dto';
@@ -24,4 +26,13 @@ export class OrdersController {
         return this.orderService.cancel(orderId);
     }
 
+    @Post('payment')
+    async paymentCallback(@Body('orderId') orderId: string, @Body('transaction') transaction: any) {
+        /*
+        const transaction = await fetch(`${paymentAPI}/orderId=${orderId}`)        
+        */
+       
+        const mockTransaction = !!(Math.floor(Math.random() * Math.floor(2)) % 2);
+        return this.orderService.paymentCallback(orderId, mockTransaction);
+    }
 }
--- src/orders/orders.service.ts
+++ src/orders/orders.service.ts
@@ -25,11 +25,48 @@ export class OrdersService {
     async cancel(orderId: string) {
         return this.orderModel.findOneAndUpdate(
             { id: orderId },
-            { state: "canceled"}
+            { state: "canceled" }
         )
-        .catch(function (err) {
-            console.error(err);
-            return err;
-        })
+            .exec()
+            .catch(function (err) {
+                console.error(err);
+                return err;
+            })
+    }
+
+    async paymentCallback(orderId: string, transaction: any) {
+        //assume result = true false, it's a mock, eh?
+        if (!transaction || !transaction.result) {
+            return this.decline(orderId);
+        }
+
+        return this.confirm(orderId);
+    }
+
+    private async confirm(orderId: string) {
+        const result = this.orderModel.findOneAndUpdate(
+            { id: orderId },
+            { state: "confirmed" }
+        ).exec();
+
+        setTimeout(() => {
+            this.orderModel.findOneAndUpdate(
+                {
+                    id: orderId,
+                    state: { $ne: "canceled" }
+                },
+                { state: "delivered" }
+            ).exec()
+        }, 1000 * Math.floor(Math.random() * Math.floor(2))
+        );
+
+        return result;
+    }
+
+    private async decline(orderId: string) {
+        return this.orderModel.findOneAndUpdate(
+            { id: orderId },
+            { state: "declined" }
+        ).exec();
     }
 }
