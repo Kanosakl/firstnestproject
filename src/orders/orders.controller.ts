a326b6c21a476e5ebbce3f7a0cc6cc13bd26b15a
Davion Teh <6722889+davionboy@users.noreply.github.com>
2020-02-20 23:23:24 +0800

    Added order create API

--- src/orders/orders.controller.ts
+++ src/orders/orders.controller.ts
@@ -1,13 +1,21 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Post, Body, UsePipes, ValidationPipe, HttpCode } from '@nestjs/common';
 import { OrdersService } from './orders.service';
 import { Order } from './order.interface';
+import { CreateOrderDto } from './create-order.dto';
 
 @Controller('orders')
 export class OrdersController {
-    constructor(private readonly orderService: OrdersService) {}
+    constructor(private readonly orderService: OrdersService) { }
 
     @Get()
     async findAll(): Promise<Order[]> {
         return this.orderService.findAll();
+    }
+
+    @Post()
+    @UsePipes(new ValidationPipe())
+    @HttpCode(201)
+    async createOrder(@Body('order') order: CreateOrderDto) {
+        return this.orderService.create(order);
     }
 }
--- src/orders/orders.service.ts
+++ src/orders/orders.service.ts
@@ -2,12 +2,23 @@ import { Injectable } from '@nestjs/common';
 import { Model } from "mongoose";
 import { InjectModel } from "@nestjs/mongoose";
 import { Order } from './order.interface';
+import { OrderSchema } from "./schemas/order.schema";
+import { CreateOrderDto } from './create-order.dto';
 
 @Injectable()
-export class OrdersService {    
-    constructor(@InjectModel('Order') private readonly orderModel: Model<Order>) {}
+export class OrdersService {
+    constructor(@InjectModel('Order') private readonly orderModel: Model<Order>) { }
 
-    async findAll(): Promise<Order[]>{
+    async findAll(): Promise<Order[]> {
         return this.orderModel.find().exec();
+    }
+
+    async create(order: CreateOrderDto) {
+        const createdOrder = new OrderSchema({
+            items: order.items,
+            state: "created",
+            email: "test1@test.com" //I'll learn how to implement authentication properly other than using Auth0 and jwt
+        });
+        return createdOrder.save();
     }
 }
